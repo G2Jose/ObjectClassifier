b59c4fe748ac7da5d2b138db25ffe7aecc0f22c8
George Jose <george.jose.a@gmail.com>
2017-06-14 23:28:56 -0400

    Initial Commit

---
+++ ObjectClassifier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ObjectClassifier
+//
+//  Created by George Jose on 2017-06-14.
+//  Copyright © 2017 g2jose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
